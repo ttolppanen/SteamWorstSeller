ed8ee3c88470f0a1cfd47378d77653bb654d2f88
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-09 22:11:53 +0300

    käsi kuvat ja kameran korjaus?

--- Assets/1Scripts/kamera.cs
+++ Assets/1Scripts/kamera.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class kamera : MonoBehaviour {
 
-	public GameObject ruumis;
-	Vector3 ruumiinPaikka;
-	public float sensitivy;
-	
-	void Update () {
-		Vector2 muutos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+    public GameObject ruumis;
+    public float katsomisRaja;
+    Vector3 ruumiinPaikka;
+    public float sensitivy;
+
+    void Update() {
+        Vector2 muutos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         transform.Rotate(new Vector3(-muutos.y, 0, 0) * sensitivy);
         ruumis.transform.Rotate(new Vector3(0, muutos.x, 0) * sensitivy);
-	}
+        if (transform.localRotation.eulerAngles.x > katsomisRaja && transform.localRotation.eulerAngles.x < 180)
+        {
+            transform.localRotation = Quaternion.Euler(katsomisRaja, 0, 0);
+        }
+        if (transform.localRotation.eulerAngles.x < (360 - katsomisRaja) && transform.localRotation.eulerAngles.x > 180)
+        {
+            transform.localRotation = Quaternion.Euler(-katsomisRaja, 0, 0);
+        }
+    }
 }
