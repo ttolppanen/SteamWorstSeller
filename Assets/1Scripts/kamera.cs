9f61900f95a065c59880ded9dea69d2269c2ebab
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-09 20:12:41 +0300

    Eka juttu

---
+++ Assets/1Scripts/kamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamera : MonoBehaviour {
+
+	public GameObject ruumis;
+	Vector3 paikkaViimeksi;
+	Vector3 ruumiinPaikka;
+	public float sensitivy;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 muutos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles + sensitivy * new Vector3 (-muutos.y, 0, 0));
+		ruumis.transform.rotation = Quaternion.Euler (ruumis.transform.rotation.eulerAngles + sensitivy * new Vector3 (0, muutos.x, 0));
+	}
+}
