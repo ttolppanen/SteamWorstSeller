9a5d1e0ec31a120e6775342241589d6c2d61e5c2
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-09 20:39:02 +0300

    Pelaajan liikkuminen

--- Assets/1Scripts/kamera.cs
+++ Assets/1Scripts/kamera.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class kamera : MonoBehaviour {
 
 	public GameObject ruumis;
-	Vector3 paikkaViimeksi;
 	Vector3 ruumiinPaikka;
 	public float sensitivy;
 	
-	// Update is called once per frame
 	void Update () {
 		Vector2 muutos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-		transform.rotation = Quaternion.Euler (transform.rotation.eulerAngles + sensitivy * new Vector3 (-muutos.y, 0, 0));
-		ruumis.transform.rotation = Quaternion.Euler (ruumis.transform.rotation.eulerAngles + sensitivy * new Vector3 (0, muutos.x, 0));
+        transform.Rotate(new Vector3(-muutos.y, 0, 0) * sensitivy);
+        ruumis.transform.Rotate(new Vector3(0, muutos.x, 0) * sensitivy);
 	}
 }
---
+++ Assets/1Scripts/pelaajanLiikkuminen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pelaajanLiikkuminen : MonoBehaviour {
+
+    public float nopeusKerroin;
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w")) {
+            rb.AddForce(transform.forward * nopeusKerroin);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(-transform.forward * nopeusKerroin);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-transform.right * nopeusKerroin);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(transform.right * nopeusKerroin);
+        }
+    }
+}
