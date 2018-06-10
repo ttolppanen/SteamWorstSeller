e6fd951ea2b5f99ea82a71f52154509aa0a2f84a
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-10 16:51:02 +0300

    Perus esineiden nosta
    Myös lisäyksiä gitIgnoreen.

---
+++ Assets/1Scripts/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject[] inventory = new GameObject[10];
+    public Transform inventorynPaikka;
+    public Transform tiputusPaikka;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i] != null)
+                {
+                    TiputaInventorysta(i);
+                    return;
+                }
+            }
+        }
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Esine" && Input.GetKey("f"))
+        {
+            Debug.Log("NOH");
+            LaitaEsineTyhjaanPaikkaan(other.gameObject);
+        }
+    }
+
+
+
+    void LaitaEsineTyhjaanPaikkaan(GameObject esine) {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if (inventory[i] == null)
+            {
+                inventory[i] = esine;
+                inventory[i].transform.parent = inventorynPaikka;
+                inventory[i].transform.position = inventorynPaikka.position;
+                inventory[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                return;
+            }
+        }
+    }
+
+    void TiputaInventorysta(int indeksi)
+    {
+        inventory[indeksi].transform.parent = null;
+        inventory[indeksi].transform.position = tiputusPaikka.position;
+        inventory[indeksi].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        inventory[indeksi] = null;
+    }
+}
