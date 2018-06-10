d7525c6cf841eb650d548b4a20d4252495ee0a5d
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-10 09:24:10 +0300

    Alustavaa vihollisten liikkumista
    -Vihollisten liikkuminen
    -Uusia kansioita

---
+++ Assets/1Scripts/katsoPelaajaPain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class katsoPelaajaPain : MonoBehaviour {
+
+    GameObject pelaaja;
+
+	void Start () {
+        pelaaja = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(pelaaja.transform);
+	}
+}
---
+++ Assets/1Scripts/vihollistenLiikuminen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vihollistenLiikuminen : MonoBehaviour {
+
+    public float liikkumisNopeus;
+    GameObject pelaaja;
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        pelaaja = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 suuntaPelaajaan = pelaaja.transform.position - transform.position;
+        suuntaPelaajaan.y = 0;
+        rb.AddForce(suuntaPelaajaan.normalized * liikkumisNopeus);
+	}
+}
