39541881d405128374906e8e54d9b0d2b8b28499
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-15 23:59:10 +0300

    Jotain testi juttuja, lähinnä miekan lyönti animaation aloitus...

---
+++ Assets/1Scripts/KarttaTesti.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarttaTesti : MonoBehaviour {
+
+    Material materiaali;
+    public GameObject vertailuReuna;
+    public float tasausKerroin;
+    GameObject pelaaja;
+
+	void Start () {
+        materiaali = GetComponent<SpriteRenderer>().material;
+        pelaaja = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void Update () {
+        Vector3 suuntaVertailusta = pelaaja.transform.position - vertailuReuna.transform.position;
+        suuntaVertailusta = new Vector2(suuntaVertailusta.x, suuntaVertailusta.z) / tasausKerroin;
+        materiaali.SetVector("keskiPaikka", suuntaVertailusta);
+	}
+}
