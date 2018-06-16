d0223d40515719c4230fcfcc887bff7e24cf0acd
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-16 22:58:38 +0300

    Dungeon1 testausta...

---
+++ Assets/1Scripts/SoihdunFlicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoihdunFlicker : MonoBehaviour {
+
+    public float flickkausAika;
+    public float flickkausMaara;
+
+    Light valo;
+    float intensityAlussa;
+    float aika;
+
+	void Start () {
+        valo = GetComponent<Light>();
+        intensityAlussa = valo.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (aika >= flickkausAika)
+        {
+            aika = 0;
+            valo.intensity = intensityAlussa + Random.Range(0f, flickkausMaara * 2) - flickkausMaara;
+        }
+        aika += Time.deltaTime;
+	}
+}
