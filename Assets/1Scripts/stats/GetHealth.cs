cfd800c4c18d13210c0bfa43f511151747117c9f
Oskari <oskari266@hotmail.com>
2018-06-18 16:39:44 +0300

    Stat Screen

---
+++ Assets/1Scripts/stats/GetHealth.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetHealth : MonoBehaviour {
+    private int stat;
+    private int statChange;
+
+    public Text text;
+    private GameObject player;
+    public statsInit.stats type;
+    private int statPool;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Cube");
+        
+
+        statsInit statFFU = player.GetComponent<statsInit>();
+        statPool = statFFU.statPool;
+        int fuck = statFFU.statsTable[type];
+        stat = fuck;
+        statChange = 0;
+        text.text = stat.ToString();
+    }
+
+    private void Update()
+    {
+        statsInit statFFU = player.GetComponent<statsInit>();
+        stat = statFFU.statsTable[type];
+        int what = stat + statChange;
+        text.text = what.ToString();
+    }
+
+    public void add()
+    {
+        player = GameObject.Find("Cube");
+        statsInit statFFU = player.GetComponent<statsInit>();
+        statPool = statFFU.statPool;
+
+        if (statPool>0)
+        {
+            stat++;
+            statChange++;
+            text.text = stat.ToString();
+            statFFU.statPool -= 1;
+            statPool--;
+        }
+    }
+
+    public void reduce()
+    {
+        if(statChange>0)
+        {
+            statsInit statFFU = player.GetComponent<statsInit>();
+            statFFU.statPool+=1;
+            stat--;
+            statChange--;
+            text.text = stat.ToString();
+            statPool++;
+        }
+    }
+
+    public void Reset()
+    {
+        stat -= statChange;
+        statChange = 0;
+        text.text = stat.ToString();
+    }
+
+    public void Allocate()
+    {
+        player.GetComponent<statsInit>().statsTable[type] += statChange;
+        statChange = 0;
+    }
+
+}
---
+++ Assets/1Scripts/stats/statsInit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class statsInit : MonoBehaviour {
+    public enum stats { Health, Strength, Intelligence };
+    public int statPool;
+    public Dictionary<stats, int> statsTable;
+
+    public void Randomize(Dictionary<stats,int> stats)
+    {
+        var values = System.Enum.GetValues(typeof(stats));
+        foreach(stats i in values)
+        {
+            stats[i] = 2;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        statsTable = new Dictionary<stats, int>();
+        statPool = 2;
+        Randomize(statsTable);
+	}
+
+    private void Update()
+    {
+
+    }
+}
