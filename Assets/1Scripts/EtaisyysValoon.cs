373dbdafe58fa8fd883814373db5985367403c84
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-06-21 17:02:12 +0300

    Goblini prefab, kadonnut etaisyysValoon scripti tehty uudestaan

---
+++ Assets/1Scripts/EtaisyysValoon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EtaisyysValoon : MonoBehaviour {
+
+    Material materiaali;
+
+	void Start () {
+        materiaali = GetComponent<SpriteRenderer>().material;
+	}
+	
+	void Update () {
+        GameObject[] valot = GameObject.FindGameObjectsWithTag("Valo");
+        materiaali.SetFloat("_EtaisyysValoon", EtsiLahinValo(valot));        
+	}
+
+
+    float EtsiLahinValo(GameObject[] valot)
+    {
+        float etaisyys = 10000;
+        foreach (GameObject valo in valot)
+        {
+            float etaisyysValoon = (transform.position - valo.transform.position).magnitude;
+            if (etaisyysValoon < etaisyys)
+            {
+                etaisyys = etaisyysValoon;
+            }
+        }
+        return etaisyys;
+    }
+}
